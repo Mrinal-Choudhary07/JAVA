941aeb0d032c7a298b0365dddabdeae24b770be4
Mrinal Choudhary <mrinal7331@gmail.com>
2020-09-09 22:46:54 +0530

    Update DeleteStudent.java

--- programs/Swing-Programs/LibraryManagementSystem/DeleteStudent.java
+++ programs/Swing-Programs/LibraryManagementSystem/DeleteStudent.java
@@ -63,7 +63,8 @@ public class DeleteStudent extends JFrame {
 
 	/**
 	 * Create the frame.
-	 * @throws Exception 
+	 * 
+	 * @throws Exception
 	 */
 	public DeleteStudent() throws Exception {
 		setResizable(false);
@@ -75,53 +76,53 @@ public class DeleteStudent extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
 		panel.setBackground(Color.WHITE);
 		panel.setBounds(0, 0, 673, 661);
 		contentPane.add(panel);
-		
+
 		JLabel lblDeletestudent = new JLabel("Delete-Student");
 		lblDeletestudent.setHorizontalAlignment(SwingConstants.CENTER);
 		lblDeletestudent.setForeground(Color.BLACK);
 		lblDeletestudent.setFont(new Font("Consolas", Font.BOLD, 40));
 		lblDeletestudent.setBounds(140, 0, 312, 78);
 		panel.add(lblDeletestudent);
-		
+
 		JSeparator separator = new JSeparator();
 		separator.setForeground(Color.BLACK);
 		separator.setBackground(Color.BLACK);
 		separator.setBounds(10, 35, 131, 2);
 		panel.add(separator);
-		
+
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setOrientation(SwingConstants.VERTICAL);
 		separator_1.setForeground(Color.BLACK);
 		separator_1.setBackground(Color.BLACK);
 		separator_1.setBounds(10, 33, 2, 611);
 		panel.add(separator_1);
-		
+
 		JSeparator separator_2 = new JSeparator();
 		separator_2.setForeground(Color.BLACK);
 		separator_2.setBackground(Color.BLACK);
 		separator_2.setBounds(10, 642, 647, 2);
 		panel.add(separator_2);
-		
+
 		JSeparator separator_1_1 = new JSeparator();
 		separator_1_1.setOrientation(SwingConstants.VERTICAL);
 		separator_1_1.setForeground(Color.BLACK);
 		separator_1_1.setBackground(Color.BLACK);
 		separator_1_1.setBounds(655, 33, 2, 611);
 		panel.add(separator_1_1);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Id card number");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1.setForeground(Color.BLACK);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1.setBounds(97, 116, 179, 30);
 		panel.add(lblNewLabel_1);
-		
+
 		idText = new JTextField();
 		idText.setForeground(SystemColor.desktop);
 		idText.setFont(new Font("Tahoma", Font.PLAIN, 17));
@@ -129,14 +130,14 @@ public class DeleteStudent extends JFrame {
 		idText.setBorder(new LineBorder(Color.BLACK, 2, true));
 		idText.setBounds(352, 116, 180, 30);
 		panel.add(idText);
-		
+
 		JLabel lblNewLabel_1_1 = new JLabel("Name");
 		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1_1.setForeground(Color.BLACK);
 		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1_1.setBounds(97, 175, 179, 30);
 		panel.add(lblNewLabel_1_1);
-		
+
 		nameText = new JTextField();
 		nameText.setBackground(Color.WHITE);
 		nameText.setEditable(false);
@@ -146,28 +147,28 @@ public class DeleteStudent extends JFrame {
 		nameText.setBorder(new LineBorder(Color.BLACK, 2, true));
 		nameText.setBounds(352, 175, 180, 30);
 		panel.add(nameText);
-		
+
 		JLabel lblNewLabel_1_2 = new JLabel("Class");
 		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1_2.setForeground(Color.BLACK);
 		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1_2.setBounds(97, 236, 179, 30);
 		panel.add(lblNewLabel_1_2);
-		
+
 		JLabel lblNewLabel_1_2_1 = new JLabel("Subjects(Group)");
 		lblNewLabel_1_2_1.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1_2_1.setForeground(Color.BLACK);
 		lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1_2_1.setBounds(97, 297, 179, 30);
 		panel.add(lblNewLabel_1_2_1);
-		
+
 		JLabel lblNewLabel_1_1_1 = new JLabel("Phone number");
 		lblNewLabel_1_1_1.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1_1_1.setForeground(Color.BLACK);
 		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1_1_1.setBounds(97, 357, 179, 30);
 		panel.add(lblNewLabel_1_1_1);
-		
+
 		phoneText = new JTextField();
 		phoneText.setBackground(Color.WHITE);
 		phoneText.setEditable(false);
@@ -177,85 +178,80 @@ public class DeleteStudent extends JFrame {
 		phoneText.setBorder(new LineBorder(Color.BLACK, 2, true));
 		phoneText.setBounds(352, 357, 180, 30);
 		panel.add(phoneText);
-		
+
 		JLabel imageLabel = new JLabel("");
 		imageLabel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		imageLabel.setBounds(352, 414, 180, 150);
-		panel.add(imageLabel);		
-		
+		panel.add(imageLabel);
+
 		JButton btnNewButton = new JButton("");
 		btnNewButton.addActionListener(new ActionListener() {
-			
 
 			public void actionPerformed(ActionEvent e) {
-String id = idText.getText();				
-				
+				String id = idText.getText();
+
 				// Check id field is empty or not?
-				if (!checkEmpty(id) )
+				if (!checkEmpty(id))
 					return;
-				
+
 				if (!validation(id))
 					return;
-				
+
 				try {
 					idNumber = Long.parseLong(id);
-					
+
 					Statement stmt = connection.createStatement();
 					ResultSet result = stmt.executeQuery("select id from addStudent");
-					long idDB=0;
+					long idDB = 0;
 					boolean idCheck = false;
-					while( result.next() )
-					{
+					while (result.next()) {
 						idDB = result.getLong("id");
-						if( ( idDB == idNumber ) )
-						{
+						if ((idDB == idNumber)) {
 							idCheck = true;
-							break;							
+							break;
 						}
 					}
-					if( idCheck ) {
+					if (idCheck) {
 						PreparedStatement ps = connection.prepareStatement("select * from addstudent where id = ?");
 						ps.setLong(1, idNumber);
-						
-					    result = ps.executeQuery();
-						result.next();						
+
+						result = ps.executeQuery();
+						result.next();
 						String nameDB = result.getString("name");
 						String classDB = result.getString("class");
 						String subjectDB = result.getString("subject");
 						long phoneDB = result.getLong("phone");
 						byte img[] = result.getBytes("image");
 						Image image = getToolkit().createImage(img);
-						
+
 						nameText.setText(nameDB);
 						classText.setText(classDB);
 						subText.setText(subjectDB);
-						phoneText.setText( String.valueOf(phoneDB) );						
-						imageLabel.setIcon( new ImageIcon(image) );
-						
+						phoneText.setText(String.valueOf(phoneDB));
+						imageLabel.setIcon(new ImageIcon(image));
+
 						btnHello.setVisible(true);
-					}
-					else {
+					} else {
 						JLabel jLabel = new JLabel("Entered ID card number is not existed.");
 						jLabel.setFont(new Font("Arial", Font.BOLD, 16));
 						jLabel.setForeground(Color.black);
-						JOptionPane.showMessageDialog(idText, jLabel);						
+						JOptionPane.showMessageDialog(idText, jLabel);
 					}
-					
-					
+
 				} catch (SQLException e1) {
 					e1.printStackTrace();
-				} 
+				}
 			}
 
 			private boolean validation(String id) {
-				String idRegex = "[\\d]+";				
+				String idRegex = "[\\d]+";
 				if (!id.matches(idRegex)) {
 					JLabel jLabel = new JLabel("Invalid ID card (must contain only numbers!)");
 					jLabel.setFont(new Font("Arial", Font.BOLD, 16));
 					jLabel.setForeground(Color.black);
 					JOptionPane.showMessageDialog(idText, jLabel);
 					return false;
-				}				
+				}
 				return true;
 			}
 
@@ -268,17 +264,18 @@ String id = idText.getText();
 					jLabel.setForeground(Color.black);
 					JOptionPane.showMessageDialog(idText, jLabel);
 					flag = false;
-				}				
+				}
 				return flag;
 			}
-			
+
 		});
 		btnNewButton.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
-		btnNewButton.setIcon(new ImageIcon(DeleteStudent.class.getResource("/MiniProjects/LibraryManagementSystem/img/enter.png")));
+		btnNewButton.setIcon(
+				new ImageIcon(DeleteStudent.class.getResource("/MiniProjects/LibraryManagementSystem/img/enter.png")));
 		btnNewButton.setBackground(Color.WHITE);
 		btnNewButton.setBounds(542, 116, 80, 30);
 		panel.add(btnNewButton);
-		
+
 		btnHello = new JButton("");
 		btnHello.setVisible(false);
 		btnHello.addActionListener(new ActionListener() {
@@ -287,43 +284,45 @@ String id = idText.getText();
 				jLabel.setFont(new Font("Arial", Font.BOLD, 16));
 				jLabel.setForeground(Color.black);
 				int check = JOptionPane.showConfirmDialog(idText, jLabel);
-				if( check == 0 )
-				{
+				if (check == 0) {
 					try {
 						PreparedStatement ps = connection.prepareStatement("delete from addstudent where id=?");
 						ps.setLong(1, idNumber);
 						ps.executeUpdate();
-						
-						jLabel = new JLabel("Student details having Id Card number "+idNumber+" is deleted");
+
+						jLabel = new JLabel("Student details having Id Card number " + idNumber + " is deleted");
 						jLabel.setFont(new Font("Arial", Font.BOLD, 16));
 						jLabel.setForeground(Color.black);
 						JOptionPane.showMessageDialog(idText, jLabel);
+						btnHello.setVisible(false);
 					} catch (SQLException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-					
-				}				
+
+				}
 			}
 		});
 		btnHello.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
-		btnHello.setIcon(new ImageIcon(DeleteStudent.class.getResource("/MiniProjects/LibraryManagementSystem/img/delStu.png")));		
+		btnHello.setIcon(
+				new ImageIcon(DeleteStudent.class.getResource("/MiniProjects/LibraryManagementSystem/img/delStu.png")));
 		btnHello.setFont(new Font("Courier New", Font.BOLD, 30));
 		btnHello.setBackground(Color.WHITE);
 		btnHello.setBounds(229, 588, 150, 40);
 		panel.add(btnHello);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon(DeleteStudent.class.getResource("/MiniProjects/LibraryManagementSystem/img/delStudent2.png")));
+		lblNewLabel_2.setIcon(new ImageIcon(
+				DeleteStudent.class.getResource("/MiniProjects/LibraryManagementSystem/img/delStudent2.png")));
 		lblNewLabel_2.setBounds(450, 0, 70, 78);
 		panel.add(lblNewLabel_2);
-		
+
 		JSeparator separator_3 = new JSeparator();
 		separator_3.setForeground(Color.BLACK);
 		separator_3.setBackground(Color.BLACK);
-		separator_3.setBounds(520, 33, 137, 4);
+		separator_3.setBounds(520, 33, 137, 2);
 		panel.add(separator_3);
-		
+
 		classText = new JTextField();
 		classText.setForeground(SystemColor.desktop);
 		classText.setFont(new Font("Tahoma", Font.PLAIN, 17));
@@ -333,7 +332,7 @@ String id = idText.getText();
 		classText.setBackground(Color.WHITE);
 		classText.setBounds(352, 236, 180, 30);
 		panel.add(classText);
-		
+
 		subText = new JTextField();
 		subText.setForeground(SystemColor.desktop);
 		subText.setFont(new Font("Tahoma", Font.PLAIN, 17));
@@ -343,13 +342,13 @@ String id = idText.getText();
 		subText.setBackground(Color.WHITE);
 		subText.setBounds(352, 297, 180, 30);
 		panel.add(subText);
-		
+
 		JLabel lblNewLabel_1_1_1_1 = new JLabel("Your photo");
 		lblNewLabel_1_1_1_1.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_1_1_1_1.setForeground(Color.BLACK);
 		lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1_1_1_1.setBounds(97, 464, 179, 30);
-		panel.add(lblNewLabel_1_1_1_1);	
-		
+		panel.add(lblNewLabel_1_1_1_1);
+
 	}
 }
